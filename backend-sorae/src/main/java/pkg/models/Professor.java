2cf4ebdc6fefa1547a995942abf062cf40caeaab
Manoele Ribeiro <86082607+ManuRss@users.noreply.github.com>
2021-12-20 16:23:33 -0300

    Classe_Professor
    Att

--- backend-sorae/src/main/java/pkg/models/Professor.java
+++ backend-sorae/src/main/java/pkg/models/Professor.java
@@ -5,17 +5,17 @@ package pkg.models;
 public class Professor {
 
     //Atributos
-private String name;
+private String nome;
 private String email;
-private String phone;
-private String occupation;
+private String telefone;
+private String ocupacao;
 
     //Construtor
-public Professor(String name, String email, String phone, String occupation) {
-    this.name = name;
+public Professor(String nome, String email, String telefone, String ocupacao) {
+    this.nome = nome;
     this.email = email;
-    this.phone = phone;
-    this.occupation = occupation;
+    this.telefone = telefone;
+    this.ocupacao = ocupacao;
 }
 
 public Professor(){
@@ -23,11 +23,11 @@ public Professor(){
 }
 
     //Métodos GET e SET
-public String getName(){
-    return name;
+public String getNome(){
+    return nome;
 }
-public void setName(String name){
-    this.name = name;
+public void setNome(String nome){
+    this.nome = nome;
 }
 
 public String getEmail(){
@@ -37,24 +37,24 @@ public void setEmail(String email){
     this.email = email;
 }
 
-public String getPhone(){
-    return phone;
+public String getTelefone(){
+    return telefone;
 }
-public void setPhone(String phone){
-    this.phone = phone;
+public void setTelefone(String telefone){
+    this.telefone = telefone;
 }
 
-public String getOccupation(){
-    return occupation;
+public String getOcupacao(){
+    return ocupacao;
 }
-public void setOccupation(String occupation){
-    this.occupation = occupation;
+public void setOcupacao(String ocupacao){
+    this.ocupacao = ocupacao;
 }
 
 @Override
 public String toString(){
-return "Nome do Professor: " + this.getName() + 
-"\n Área de Atuação: " + this.getOccupation();
+return "Nome do Professor: " + this.getNome() + 
+"\n Área de Atuação: " + this.getOcupacao();
 }
 
 }
