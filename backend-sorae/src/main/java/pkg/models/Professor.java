e064cfc6eb0c327e79d09fc66adf62b91ed06246
Manoele Ribeiro <86082607+ManuRss@users.noreply.github.com>
2021-12-21 17:07:44 -0300

    ModelProfessor-att
    inserção de Id

--- backend-sorae/src/main/java/pkg/models/Professor.java
+++ backend-sorae/src/main/java/pkg/models/Professor.java
@@ -1,18 +1,27 @@
 package pkg.models;
 
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Professor {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     //Atributos
-private String nome;
-private String email;
-private String telefone;
-private String ocupacao;
+    private String nome;
+    private String idprofessor;
+    private String email;
+    private String telefone;
+    private String ocupacao;
 
     //Construtor
-public Professor(String nome, String email, String telefone, String ocupacao) {
+public Professor(String nome, String idprofessor, String email, String telefone, String ocupacao) {
     this.nome = nome;
+    this.idprofessor = idprofessor;
     this.email = email;
     this.telefone = telefone;
     this.ocupacao = ocupacao;
@@ -28,6 +37,14 @@ public String getNome(){
 }
 public void setNome(String nome){
     this.nome = nome;
+}
+
+public String getIdprofessor(){
+    return idprofessor;
+}
+
+public void setIdprofessor(String idprofessor){
+    this.idprofessor = idprofessor;
 }
 
 public String getEmail(){
