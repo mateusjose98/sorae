5182c4191b038243b7cd0458cede34afeecb97c4
Fernanda <fernandam04@outlook.com>
2021-12-21 17:26:08 -0300

    Implementação do Crud da Entidade Aluno. Sem tratamento de erro

--- backend-sorae/src/main/java/pkg/repositories/AtividadeRepository.java
+++ backend-sorae/src/main/java/pkg/repositories/AtividadeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import pkg.models.Atividade;
 
+import java.util.Optional;
+
 public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
 
 }
